e7de4dc97620381e23c0871e5706984d743fc198
BeniVF <beni.villa@gmail.com>
2014-08-09 19:13:23 +0100

    Add building configuration to set scala.

---
+++ payroll-core/project/PayrollBuild.scala
@@ -0,0 +1,25 @@
+import sbt._
+import Keys._
+
+object PayrollBuild extends Build {
+  val Organization = "anova"
+  val Name = "payroll-core"
+  val Version = "0.1.0-SNAPSHOT"
+  val ScalaVersion = "2.11.1"
+  val ScalaTestVersion = "2.2.1"
+
+  lazy val project = Project(
+    Name,
+    file("."),
+    settings =
+      Defaults.defaultSettings ++
+        Seq(
+          organization := Organization,
+          name := Name,
+          version := Version,
+          scalaVersion := ScalaVersion,
+          javacOptions ++= Seq("-source", "1.7"),
+          libraryDependencies += "iorg.scalatest" %% "scalatest" % ScalaTestVersion % "test"
+        )
+  )
+}
